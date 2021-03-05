0fafbd3ba7316cb5e9b9cc29761eceff451e9616
Renan Freitas <renan028@gmail.com>
2021-03-05 15:32:48 -0300

    change to work woth webots_cpp

--- launch/forklift.launch.py
+++ launch/forklift.launch.py
@@ -32,10 +32,9 @@ def generate_launch_description():
     webots = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('webots_ros2_core'), 
-                'launch', 'robot_launch.py')
+                'launch', 'webots_launch.py')
         ),
         launch_arguments={
-            'executable': '',
             'world': os.path.join(robot_dir, 'forklift_webots', 'worlds', 
                 'warehouse.wbt'),
             'output': 'screen'
